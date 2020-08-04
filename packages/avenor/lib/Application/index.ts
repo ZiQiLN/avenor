18f1541eb3657dd45a2db6e49ce47df90c476d9c
Jakub Olan <jakub.olan001@gmail.com>
2020-08-05 01:00:13 +0200

    docs: updated plans for main file

--- packages/avenor/lib/Application/index.ts
+++ packages/avenor/lib/Application/index.ts
@@ -4,10 +4,9 @@ import http2 from 'http2'
 
 import getPort from 'get-port'
 import signale from 'signale'
-import _ from 'lodash'
+import _ from 'lodash' // We need to get rid of that shit
 
 import debug from 'debug'
-import { EventEmitter } from 'events'
 
 // TODO(#8): Main application Class which should have all important methods that are described in https://github.com/ZiQiLN/avenor/issues/8
 
@@ -17,17 +16,31 @@ import { EventEmitter } from 'events'
 // https://github.com/l2ig/aqua
 // https://github.com/fastrodev/fastro
 
-export class Avenor {
-	// Implementation of Server type which is related to http.Server
-	server: http.Server | undefined
+debug('avenor:constructor')
 
-	constructor() {}
+export class Avenor {
+	// Method that holds an http.Server on which application will be based, I have no idea actually how to implement http2-based server, I'll do more research on this idea.
+	server: http.Server | undefined
+	// TODO(#6): Router Implementation
+	router: undefined
+	// TODO(#8) Add Middleware Support
+	middleware: undefined
+	// TODO(#8): Add Pre-Middleware Support
+	premiddleware: undefined
+
+	// Namespace for defining private processes that going to be used inside class functions in asynchronic way.
+	#process: Promise<void> | undefined
 
 	// TODO: use() function will be used to include a new middleware of application, optionally we'll work for a router support, to implement.
 	public use() {}
+	// TODO(): Private function to apply middleware to application class, in asynchronic loop, that will be fast method I think. Function should work with two parameters, first one for handler function and second one for array containing middleware functions.
+	private applyMiddleware(h: undefined, m: undefined) {}
 	// TODO: listen() function should run application on specified PORT and allow user to include callback function. This will be probably used at production because we have startup() command for development enviroment.
 	// NOTE: Worthly thing to note is implementation of **http2** server inside application which can be impactful on performance in large applications, also we can think about clustering but implementation of clustering at library-level can potentially broke application.
 	public listen() {}
 	// NOTE: Similar function to listen() but this one, runs application automatically without configuration of PORT by default preferable port (1337) and includes callback. If PORT 1337 is taken application will automatically find one and run on random free port. Usefull for development not much at production.
-	public async startup() {}
+	private async start(s: http.Server): Promise<void> {
+		this.server = s
+		// TODO: Create a new Context which will implement middlewares
+	}
 }
